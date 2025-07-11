0a181e9fe345a6179e89df8dab98a80b936f6f7f
kelly saldaña <kellysalb@hotmail.com>
2025-07-11 17:24:08 -0500

    fix: cambio de importación para Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import VistaPedidoPage from './Pages/VistaPedidoPage';
+import VistaPedidoPage from './Pages/vistaPedidoPage';
 
 function App() {
   return (
